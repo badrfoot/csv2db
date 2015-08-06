d5d396bc5e103c0a7fdf131960ce9004b19fb3b3
bass0n <bass0n@b0>
2015-08-06 18:42:32 -0400

    Added constructor with parameters and override for toString() method

---
+++ src/java/csv/Csv2Database.java
@@ -0,0 +1,16 @@
+package csv;
+
+import java.io.*;
+import java.util.*;
+/*import java.util.logging.*;
+import javax.servlet.annotation.*;*/
+
+
+public class Csv2Database {
+    
+    ArrayList<Person> parsedPersons;
+    ArrayList<Person> addedPersons;
+    public ArrayList<Person> readCsv(InputStream is) throws IOException{
+        return parsedPersons;
+    }
+}
